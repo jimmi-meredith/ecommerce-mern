c788f7dbd3df185054ef14b505b82ad46181c8a8
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-08 18:15:01 +1300

    Edit footer details to incuilde copyright

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to J-Shop</h1>
+    <div className='App'>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to J-Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   )
 }
