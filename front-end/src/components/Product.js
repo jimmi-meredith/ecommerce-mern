59ef81008cf8b104d4b424047a84787c6f11d3d1
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-08 19:16:23 +1300

    display product price

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -17,6 +17,7 @@ const Product = ({ product }) => {
               {product.rating} from {product.numReviews} reviews
             </div>
           </Card.Text>
+          <Card.Text as='h3'>${product.price}</Card.Text>
         </a>
       </Card.Body>
     </Card>
