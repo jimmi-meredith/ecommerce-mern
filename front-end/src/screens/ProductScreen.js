8ed9a01dd62b4b01c8133c90c17a907f4281d2c0
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-09 13:29:03 +1300

    create go back button in product page

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
-  return <div>Product</div>
+const ProductScreen = ({ match }) => {
+  const product = products.find((product) => product._id === match.params.id)
+  return (
+    <div>
+      <Link className='btn btn-dark my-3' to='/'>
+        Go back
+      </Link>
+    </div>
+  )
 }
 
 export default ProductScreen
