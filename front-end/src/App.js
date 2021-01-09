ee35b9bcc29b53593c59edb3c19c6a0d8659c93e
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-09 13:11:17 +1300

    add route for product pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Homescreen from './screens/Homescreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route exact path='/' component={Homescreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
