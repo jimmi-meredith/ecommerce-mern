af6e3960bf6059cc16e0ea4b3da55d0daee39bda
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-13 12:53:45 +1300

    create middleware file

--- back-end/server.js
+++ back-end/server.js
@@ -18,15 +18,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  })
-})
-
 app.listen(
   PORT,
   console.log(
