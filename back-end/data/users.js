2a7e7af6665768675703fc7be1d4ffe8a0704c73
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-12 21:52:55 +1300

    create sample users data

---
+++ back-end/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: xxx,
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: xxx,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: xxx,
+  },
+]
