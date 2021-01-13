f24a817fd94204b410d36f5fe0711ab95b136938
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-14 12:32:52 +1300

    create CartScreen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Homescreen from './screens/Homescreen'
+import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
@@ -12,8 +13,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route exact path='/' component={Homescreen} />
+          <Route exact path='/' component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
