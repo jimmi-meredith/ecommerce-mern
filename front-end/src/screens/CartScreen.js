d832af40dcd73e6de3cd0605175dc434256875a5
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-15 09:21:16 +1300

    fill out cartScreen info

--- front-end/src/screens/CartScreen.js
+++ front-end/src/screens/CartScreen.js
@@ -18,7 +18,63 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, quantity])
 
-  return <div>Cart</div>
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    {' '}
+                    <Form.Control
+                      as='select'
+                      value={quantity}
+                      onChange={(event) => {
+                        dispatch(
+                          addToCart(item.product, Number(event.target.value))
+                        )
+                      }}
+                    >
+                      {[...Array(item.countInStock).keys()].map((key) => (
+                        <option key={key + 1} value={key + 1}>
+                          {key + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={2}></Col>
+      <Col md={2}></Col>
+    </Row>
+  )
 }
 
 export default CartScreen
