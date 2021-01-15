9dd4587f4fa2e5517633c1a1595320995fe59df4
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-16 12:53:48 +1300

    compare user passwords with one in database

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -4,7 +4,17 @@ import User from '../models/userModel.js'
 // POST - /api/users/login
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
-  res.send({ email, password })
+  const user = await User.findOne({ email })
+  user && (await user.matchPassword(password))
+    ? res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: null,
+      })
+    : res.status(401)
+  throw new Error('Invalid email or password')
 })
 
 export { authUser }
