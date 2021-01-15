b803fd8c8e18cb76edfcb09766bc4a288fde98b7
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-16 12:55:38 +1300

    convert to if statement

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -5,16 +5,18 @@ import User from '../models/userModel.js'
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
-  user && (await user.matchPassword(password))
-    ? res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: null,
-      })
-    : res.status(401)
-  throw new Error('Invalid email or password')
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
 })
 
 export { authUser }
