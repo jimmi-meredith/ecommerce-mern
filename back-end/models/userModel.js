7121c0615d4ce8cf3373d767755f3174da762435
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-16 13:20:13 +1300

    get this checked

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -27,8 +27,9 @@ const userSchema = mongoose.Schema(
   }
 )
 
-userSchema.methods.matchPassword = async (enteredPassword) =>
+userSchema.methods.matchPassword = async (enteredPassword) => {
   await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
