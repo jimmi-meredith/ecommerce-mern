ce2716f4fa91872459358b561a1c068ce6178960
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-18 15:47:07 +1300

    create generateToken function:

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // POST - /api/users/login
@@ -11,22 +12,10 @@ const authUser = asyncHandler(async (req, res) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: null,
+        token: generateToken(user._id),
       })
     : res.status(401)
   throw new Error('Invalid email or password')
-  // if (user && (await user.matchPassword(password))) {
-  //   res.json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     isAdmin: user.isAdmin,
-  //     token: null,
-  //   })
-  // } else {
-  //   res.status(401)
-  //   throw new Error('Invalid email or password')
-  // }
 })
 
 export { authUser }
