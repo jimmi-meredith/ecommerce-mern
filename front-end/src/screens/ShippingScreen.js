4461940be55d0235a8dc204b1703ffd0a992b6f5
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-21 13:57:11 +1300

    Add ability to save address state to store

--- front-end/src/screens/ShippingScreen.js
+++ front-end/src/screens/ShippingScreen.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart)
+
   const { shippingAddress } = cart
 
   const [address, setAddress] = useState(shippingAddress.address)
@@ -12,9 +14,12 @@ const ShippingScreen = ({ history }) => {
   const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
   const [country, setCountry] = useState(shippingAddress.country)
 
+  const dispatch = useDispatch()
+
   const submitHandler = (event) => {
     event.preventDefault()
-    console.log('submit')
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    history.push('/payment')
   }
 
   return (
