d6a2a1737cf7aba1f914d369c3854c8aaecf0324
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-22 09:36:03 +1300

    add nav items for each payment step

--- front-end/src/components/CheckoutSteps.js
+++ front-end/src/components/CheckoutSteps.js
@@ -7,11 +7,38 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
     <Nav className='justify=content-center mb-4'>
       <Nav.Item>
         {step1 ? (
-          <LinkContainer to='login'>
+          <LinkContainer to='/login'>
             <Nav.Link>Sign In</Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disalbed>Sign In</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step2 ? (
+          <LinkContainer to='/shipping'>
+            <Nav.Link>Shipping</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disalbed>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step3 ? (
+          <LinkContainer to='/payment'>
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disalbed>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step4 ? (
+          <LinkContainer to='/placeorder'>
+            <Nav.Link>Place Order</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disalbed>Place Order</Nav.Link>
         )}
       </Nav.Item>
     </Nav>
