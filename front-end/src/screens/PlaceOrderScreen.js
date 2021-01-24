eddd1f2b288b54e346bfbdf5a7a998a4f980fd4d
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-25 08:47:39 +1300

    calculate shipping and tax price

--- front-end/src/screens/PlaceOrderScreen.js
+++ front-end/src/screens/PlaceOrderScreen.js
@@ -12,6 +12,8 @@ const PlaceOrderScreen = () => {
     (acc, item) => acc + item.price * item.quantity,
     0
   )
+  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 10
+  cart.taxPrice = Number((0.15 * cart.itemsPrice).toFixed(2))
 
   const placeOrderHandler = () => {
     console.log('Place Order')
