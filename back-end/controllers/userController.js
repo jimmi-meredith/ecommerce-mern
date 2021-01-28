87d5ac49b178be123d467489211c60367b874826
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-01-29 10:40:42 +1300

    create update user controller

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -67,8 +67,8 @@ const getUserProfile = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
   if (user) {
-    ;(user.name = req.body.name || user.name),
-      (user.email = req.body.email || user.email)
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
     if (req.body.password) {
       user.password = req.body.password
     }
@@ -115,6 +115,26 @@ const getUserById = asyncHandler(async (req, res) => {
   }
 })
 
+// PUT - /api/users/:id
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+    const updatedUser = await user.save()
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
   authUser,
   registerUser,
@@ -123,4 +143,5 @@ export {
   getUsers,
   deleteUser,
   getUserById,
+  updateUser,
 }
