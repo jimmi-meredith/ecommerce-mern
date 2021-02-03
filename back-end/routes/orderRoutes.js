3749676edd75b9d947a28e941ea829c25b391abe
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-02-04 09:54:05 +1300

    create order list screen

--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -6,7 +6,7 @@ import {
   getMyOrders,
   getOrders,
 } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
