48a2dbb63726b46e8bfec19211ee8c2cc32a15d7
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-02-04 09:27:53 +1300

    create get orders controller

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -71,4 +71,16 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
+// GET - /api/orders
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+}
