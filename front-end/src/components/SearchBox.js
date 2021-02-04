9b8fc351203184e0c754bff079bb59360b289a80
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-02-05 11:49:28 +1300

    add saerch bar to header

--- front-end/src/components/SearchBox.js
+++ front-end/src/components/SearchBox.js
@@ -14,7 +14,7 @@ const SearchBox = ({ history }) => {
   }
 
   return (
-    <Form onSubmit={submitHandler} inLine>
+    <Form onSubmit={submitHandler} inline>
       <Form.Control
         type='text'
         name='q'
@@ -22,7 +22,7 @@ const SearchBox = ({ history }) => {
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button type='submit' varaint='outline-success' className='p-2'>
+      <Button type='submit' variant='outline-success' className='p-2'>
         Search
       </Button>
     </Form>
