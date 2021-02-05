80c1a4d4cce0b6fe698273c3ea9a889f144ef037
Jimmi Meredith <jimmi.meredith@gmail.com>
2021-02-05 23:56:40 +1300

    set up for deployment

--- back-end/server.js
+++ back-end/server.js
@@ -24,10 +24,6 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
-  res.send('API is Running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -40,6 +36,17 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/front-end/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is Running...')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
